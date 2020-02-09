c6b186ada7562dcbe135239eb97c702568029cfe
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-02-09 06:42:38 -0600

    fix merge conflict

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import { Todo } from './modules/todo_modules/todo';
-import { Project } from './modules/todo_modules/project';
-import { DOMFactory } from './modules/ui/dom_factory';
-import './style/app.scss';
+import { Todo } from "./modules/todo_modules/todo";
+import { Project } from "./modules/todo_modules/project";
+import { DOMFactory } from "./modules/ui/dom_factory";
+import "./style/app.scss";
 
-console.log('Webpack is working like charm !');
+console.log("Webpack is working like charm !");
 
-let todoProject = JSON.parse(window.localStorage.getItem('projects'));
+let todoProject = JSON.parse(window.localStorage.getItem("projects"));
 if (todoProject == null) {
   todoProject = [];
 }
 
 function updateLocalStorage(array) {
-  window.localStorage.setItem('projects', JSON.stringify(array));
+  window.localStorage.setItem("projects", JSON.stringify(array));
 }
 
-const todo = new Todo('', '', '', '');
-const project = new Project('Coding');
+const todo = new Todo("", "", "", "");
+const project = new Project("Coding");
 const dom = new DOMFactory();
 
-const btnProject = document.getElementById('add-new-project');
-const projectList = document.getElementById('project-list');
+const btnProject = document.getElementById("add-new-project");
+const projectList = document.getElementById("project-list");
 
-function temporaryCreateProject(){
-  const newProjectElement = document.createElement('div');
-  newProjectElement.textContent = 'New Project';
+function temporaryCreateProject() {
+  const newProjectElement = document.createElement("div");
+  newProjectElement.textContent = "New Project";
 
   projectList.appendChild(newProjectElement);
 }
 
-btnProject.addEventListener('click', temporaryCreateProject);
+btnProject.addEventListener("click", temporaryCreateProject);
